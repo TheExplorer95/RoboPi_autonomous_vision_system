765a47d6425343fecb1b0c27bfcec3b7ba0d577a
TheExplorer <j.bajorath@outlook.de>
2020-06-18 23:04:38 +0200

    PS3 controller connected to RaspberryPi via bluetooth; PWM for wheels; Calculations for steering

--- l298n_driver.py
+++ l298n_driver.py
@@ -1,94 +1,192 @@
 # Python script to control the L298N-Servo
 
-import RPi.GPIO as GPIO
-import time
+import pygame
+import math
+from sys import exit
+from gpiozero import Motor
+from time import sleep
 
-# Global variables
+#------------Global variables-----------------
+
+# set GPIO PINs by 'Broadcom SOC channel'
 
 # left front wheel
-lf_en = 4
-lf_in1 = 17
-lf_in2 = 27
+lf_bw_PIN = 27
+lf_fw_PIN = 22
 
 # rigth front wheel
-rf_in3 = 22
-rf_in4 = 5
-rf_en = 6
+rf_bw_PIN = 19
+rf_fw_PIN = 13
 
 # left back wheel
-lb_en = 13
-lb_in1 = 19
-lb_in2 = 26
+lb_bw_PIN = 4
+lb_fw_PIN = 17
 
 # rigth back wheel
-rb_in3 = 18
-rb_in4 = 23
-rb_en = 24
+rb_bw_PIN = 18
+rb_fw_PIN = 26
 
-def setupGPIO():
-    # set GPIO to 'Broadcom SOC channel'
-    GPIO.setmode(GPIO.BCM)
+# initialize the motors
+bl = Motor(forward=lf_fw_PIN, 
+              backward=lf_bw_PIN,
+              enable=None,
+              pwm=True)
 
-    # Setting all pins as output
-    GPIO.setup(lf_in1, GPIO.OUT)
-    GPIO.setup(lf_in2, GPIO.OUT)
-    GPIO.setup(rf_in3, GPIO.OUT)
-    GPIO.setup(rf_in4, GPIO.OUT)
-    GPIO.setup(lb_in1, GPIO.OUT)
-    GPIO.setup(lb_in2, GPIO.OUT)
-    GPIO.setup(rb_in3, GPIO.OUT)
-    GPIO.setup(rb_in4, GPIO.OUT)
-    
-    GPIO.setup(lf_en, GPIO.OUT)
-    GPIO.setup(rf_en, GPIO.OUT)
-    GPIO.setup(lb_en, GPIO.OUT)
-    GPIO.setup(rb_en, GPIO.OUT)
-    
-    # GPIO preset
-    GPIO.output(lf_in1, GPIO.LOW)
-    GPIO.output(lf_in2, GPIO.LOW)
-    GPIO.output(rf_in3, GPIO.LOW)
-    GPIO.output(rf_in4, GPIO.LOW)
-    GPIO.output(lb_in1, GPIO.LOW)
-    GPIO.output(lb_in2, GPIO.LOW)
-    GPIO.output(rb_in3, GPIO.LOW)
-    GPIO.output(rb_in4, GPIO.LOW)
-    
-    GPIO.output(lf_en, GPIO.HIGH)
-    GPIO.output(rf_en, GPIO.HIGH)
-    GPIO.output(lb_en, GPIO.HIGH)
-    GPIO.output(rb_en, GPIO.HIGH)
+fr = Motor(forward=rf_fw_PIN, 
+              backward=rf_bw_PIN,
+              enable=None,
+              pwm=True)
+  
+fl = Motor(forward=lb_fw_PIN, 
+              backward=lb_bw_PIN,
+              enable=None,
+              pwm=True)
+  
+br = Motor(forward=rb_fw_PIN, 
+              backward=rb_bw_PIN,
+              enable=None,
+              pwm=True)
 
-def forward():
-    GPIO.output(lf_in1, GPIO.LOW)
-    GPIO.output(lf_in2, GPIO.HIGH)
-    GPIO.output(rf_in1, GPIO.LOW)
-    GPIO.output(rf_in2, GPIO.HIGH)
-    GPIO.output(lb_in1, GPIO.LOW)
-    GPIO.output(lb_in2, GPIO.HIGH)
-    GPIO.output(rb_in1, GPIO.LOW)
-    GPIO.output(rb_in2, GPIO.HIGH)
+# initialize controler
+try:
+    pygame.init()
+    window = pygame.display.set_mode((200, 200), 0, 32)
+    pygame.joystick.init()
+    if not pygame.joystick.get_count(): print('no joystick')
+    j = pygame.joystick.Joystick(0)
+    j.init()
+    print(f'Initialized: {j.get_name()}')
+except:
+    pygame.joystick.quit()
+    exit()
 
+# key mappings
 
-def backward():
-    GPIO.output(lf_in1, GPIO.HIGH)
-    GPIO.output(lf_in2, GPIO.LOW)
-    GPIO.output(rf_in1, GPIO.HIGH)
-    GPIO.output(rf_in2, GPIO.LOW)
-    GPIO.output(lb_in1, GPIO.HIGH)
-    GPIO.output(lb_in2, GPIO.LOW)
-    GPIO.output(rb_in1, GPIO.HIGH)
-    GPIO.output(rb_in2, GPIO.LOW)
+PS3_axis_left_h = 0
+PS3_axis_left_v = 1 
+PS3_axis_right_h = 2 
 
-def endRoutine():
-    GPIO.cleanup()
-    print('GPIO\'s cleaned up')
+#def mv_fw():
+#    GPIO.output(lf_bw, GPIO.LOW)
+#    GPIO.output(lf_fw, GPIO.HIGH)
+#    GPIO.output(rf_bw, GPIO.LOW)
+#    GPIO.output(rf_fw, GPIO.HIGH)
+#    GPIO.output(lb_bw, GPIO.LOW)
+#    GPIO.output(lb_fw, GPIO.HIGH)
+#    GPIO.output(rb_bw, GPIO.LOW)
+#    GPIO.output(rb_fw, GPIO.HIGH)
+#def mv_bw():
+#    GPIO.output(lf_bw, GPIO.HIGH)
+#    GPIO.output(lf_fw, GPIO.LOW)
+#    GPIO.output(rf_bw, GPIO.HIGH)
+#    GPIO.output(rf_fw, GPIO.LOW)
+#    GPIO.output(lb_bw, GPIO.HIGH)
+#    GPIO.output(lb_fw, GPIO.LOW)
+#    GPIO.output(rb_bw, GPIO.HIGH)
+#    GPIO.output(rb_fw, GPIO.LOW)
+#def turn_r(dutyCycle):
+#    lf.forward(dutyCycle)
+#    rf.backward(dutyCycle)
+#    lb.forward(dutyCycle)
+#    rb.backward(dutyCycle)
+#
+#def turn_l(dutyCycle):
+#    lf.backward(dutyCycle)
+#    rf.forward(dutyCycle)
+#    lb.backward(dutyCycle)
+#    rb.forward(dutyCycle)
+
+def test():
+    fl.forward(.5)
+    sleep(2)
+    fr.forward(.5)
+    sleep(2)
+    bl.forward(.5)
+    sleep(2)
+    br.forward(.5)
+    sleep(2)
+
 
 def main():
-    setupGPIO()
-    forward()
-    time.sleep(5)
-    endRoutine()
+    try:
+        while True:
+            pygame.event.get()
+            # get the joystick values
+            joystick_left_h = j.get_axis(PS3_axis_left_h)
+            joystick_left_v = j.get_axis(PS3_axis_left_v)
+            joystick_right_h = j.get_axis(PS3_axis_right_h)
+
+            if joystick_left_v != 0:
+                joystick_left_v = -joystick_left_v
+
+            if (joystick_left_h != 0) or (joystick_left_v != 0) or (joystick_right_h != 0):
+                # calculate desired speed (magnitude) [-1,1]
+                speed = math.sqrt(joystick_left_h**2 + joystick_left_v**2)/1.39
+
+                # calculate desired angle in rad [0, 2pi]
+                vec_x = joystick_left_h 
+                vec_y = joystick_left_v 
+                len_vec = math.sqrt(vec_x*vec_x + vec_y*vec_y)
+                
+                if len_vec != 0:
+                    angle = math.asin(vec_y/len_vec)*180/math.pi
+                else:
+                    angle = 0
+
+                if joystick_left_h >= 0 and angle >= 0:
+                    angle = angle
+                elif joystick_left_h >= 0 and angle < 0:
+                    angle = 360 + angle
+                elif joystick_left_h < 0 and angle >= 0:
+                    angle = 180 - angle
+                elif joystick_left_h < 0 and angle < 0:
+                    angle = 180 - angle
+                
+                angle = ((angle-90)%360) *math.pi/180
+
+                # calculate desired rotation
+                rotation = joystick_right_h
+
+                # calculate vectors
+                v_fl = speed * math.sin(angle + (math.pi/4)) + rotation
+                v_fr = speed * math.cos(angle + (math.pi/4)) - rotation
+                v_bl = speed * math.cos(angle + (math.pi/4)) + rotation
+                v_br = speed * math.sin(angle + (math.pi/4)) - rotation
+                
+                print(f'FL={v_fl} FR={v_fr} BL={v_bl} BR={v_br}')
+
+                # update motor
+                if v_fl < 0:
+                    fl.backward(abs(v_fl))
+                else:
+                    fl.forward(v_fl)
+                
+                if v_fr < 0:
+                    fr.backward(abs(v_fr))
+                else:
+                    fr.forward(v_fr)
+                
+                if v_bl < 0:
+                    bl.backward(abs(v_bl))
+                else:
+                    bl.forward(v_bl)
+                
+                if v_br < 0:
+                    br.backward(abs(v_br))
+                else:
+                    br.forward(v_br)
+            else:
+                fl.stop()
+                fr.stop()
+                bl.stop()
+                br.stop()
+                
+            
+            sleep(.1)
+
+    except KeyboardInterrupt:
+        pygame.joystick.quit()
+        exit()
 
 if __name__ == '__main__':
     main()
