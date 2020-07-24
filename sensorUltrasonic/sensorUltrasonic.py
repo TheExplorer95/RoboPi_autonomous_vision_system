707bd1a62145ab7fe6c66b47cf92e2e4502c506b
TheExplorer <j.bajorath@outlook.de>
2020-07-24 13:14:04 +0200

    Ultrasonic sensor script for all sensors

--- sensorUltrasonic/sensorUltrasonic.py
+++ sensorUltrasonic/sensorUltrasonic.py
@@ -1,23 +1,22 @@
 from gpiozero import DistanceSensor
 from time import sleep
-from sys import quit
 
 # Setting HC-SR04 range [0.02 m, 4 m]
 max_distance = 4
 min_distance = 0.02
 
 # Initializing the PINS for the sensors
-echo_sensor_side_left =
-trigger_sensor_side_left =
+echo_sensor_side_left = 19
+trigger_sensor_side_left = 13
 
-echo_sensor_side_right =
-trigger_sensor_side_right =
+echo_sensor_side_right = 6
+trigger_sensor_side_right = 5
 
-echo_sensor_back_left =
-trigger_sensor_back_left =
+echo_sensor_back_left = 22
+trigger_sensor_back_left = 27
 
-echo_sensor_back_right =
-trigger_sensor_back_right =
+echo_sensor_back_right = 17
+trigger_sensor_back_right = 4
 
 # Initializing the sensor variables
 sensor_side_left = DistanceSensor(echo=echo_sensor_side_left,
@@ -40,6 +39,7 @@ sensor_back_right = DistanceSensor(echo=echo_sensor_back_right,
 def main():
     try:
         while True:
+            sleep(2)
             print('HR-SR04 distances:')
             print(f'Side left - {sensor_side_left.distance} m')
             print(f'Side right - {sensor_side_right.distance} m')
