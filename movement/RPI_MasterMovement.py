ed6d36e2645f8c3050fd12b9a01784ad815b4d0e
TheExplorer <j.bajorath@outlook.de>
2020-08-05 18:30:28 +0200

    float arrays can be sent

--- movement/RPI_MasterMovement.py
+++ movement/RPI_MasterMovement.py
@@ -7,14 +7,19 @@ arduino = 0x03
 # initialize serial commumication
 bus = smbus.SMBus(1)
 
-def writeNumber(value):
+def writeNumbers(values):
     # sending an float to the arduino
     # consider changing d to f for float precision
-    ba = list(struct.pack('f', value))
-    print(ba)
-    bus.write_i2c_block_data(arduino, ba[0], ba[1:4])
+    byteList = []
+    for value in values:
+        byteList += list(struct.pack('f', value))
+    byteList.append(0)  # fails to send last byte over I2C, hence this needs to be added 
+    bus.write_i2c_block_data(arduino, byteList[0], byteList[1:12])
 
-while True:
-    number = float(input('Give me a number: '))
-    writeNumber(number)
+if __name__ == '__main__':
+    while True:
+        numbers = []
+        for i in range(3):
+            numbers.append(float(input(f'Give me number {i}: ')))
+        writeNumbers(numbers)
 
