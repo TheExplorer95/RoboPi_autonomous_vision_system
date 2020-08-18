1ef62ec85c55ba1e0a0999bd772cf79558ce73e6
TheExplorer <j.bajorath@outlook.de>
2020-08-18 12:24:48 +0200

    movement via PS3 controller working

--- movement/RPI_MasterMovement.py
+++ movement/RPI_MasterMovement.py
@@ -11,10 +11,13 @@ def writeNumbers(values):
     # sending an float to the arduino
     # consider changing d to f for float precision
     byteList = []
-    for value in values:
-        byteList += list(struct.pack('f', value))
-    byteList.append(0)  # fails to send last byte over I2C, hence this needs to be added 
-    bus.write_i2c_block_data(arduino, byteList[0], byteList[1:12])
+    try:
+        for value in values:
+            byteList += list(struct.pack('f', value))
+        byteList.append(0)  # fails to send last byte over I2C, hence this needs to be added 
+        bus.write_i2c_block_data(arduino, byteList[0], byteList[1:12])
+    except:
+        return 0
 
 if __name__ == '__main__':
     while True:
--- movement/l298n_driver.py
+++ movement/l298n_driver.py
@@ -1,6 +1,6 @@
 #Python script to read the Joystick values and send them to the Arduino
 
-import RPI_MasterMovement
+import RPI_MasterMovement as Send
 import pygame
 import math
 from time import sleep
@@ -14,7 +14,7 @@ try:
     pygame.joystick.init()
     j = pygame.joystick.Joystick(0)
     j.init()
-    print(fi'[INFO] Initialized: {j.get_name()}')
+    print(f'[INFO] Initialized: {j.get_name()}')
 except:
     print('[ERROR] No Joystick connected')
     pygame.joystick.quit()
@@ -71,9 +71,9 @@ if __name__ == '__main__':
     try:
         while True:
             # send numbers to the Arduino
-            writeNumbers(getJoystickReading())
+            Send.writeNumbers(getJoystickReading())
 
-            sleep(.1)
+            sleep(.01)
 
     except KeyboardInterrupt:
         pygame.joystick.quit()
